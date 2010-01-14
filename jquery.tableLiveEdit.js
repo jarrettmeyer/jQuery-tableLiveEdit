f34a20fb3088c4d2e06ef43a1a2011bd85227dca
Jarrett Meyer <jarrettmeyer@gmail.com>
2010-01-13 22:12:22 -0500

    cleaned up options, added options documentation

--- jquery.tableLiveEdit.js
+++ jquery.tableLiveEdit.js
@@ -1,9 +1,8 @@
 (function($) {
-
-
     $.fn.tableLiveEdit = function(settings) {
         var config = {
             formId: "_tableLiveEdit",           // ID of the form created by the plugin
+            modelId: "id",                      // ID of the element being added or edited (possibly a hidden field)
 
             addLink: "add-link", 		        // ID for the add link
             cancelButton: "live-edit-cancel",   // ID for the cancel button (generated)
@@ -16,7 +15,8 @@
             getNew: "./new",        // GET url path to return a new row edit template
             getRow: "./show",       // GET url path to return a single row
             postCreate: "./create", // POST url path to create a new row
-            postUpdate: "./udpate", // POST url path to update an existing row
+            putUpdate: "./udpate",  // PUT url path to update an existing row
+            usePostUpdate: false,   // Should the update URL be called with a POST request, instead of a PUT request
 
             useHighlight: false
         };
@@ -35,14 +35,39 @@
                 }
             };
             var registerAddLink = function() {
-                $("#" + config.addLink).click(function() {
+                $("#" + config.addLink).unbind("click");
+                $("#" + config.addLink).click(function(e) {
+                    e.preventDefault();
+                    hideEditLinks();
+                    $.get(config.getNew, {}, function(html) {
+                        $("tbody").append(html);
+                        registerCreateButton();
+                        registerCancelButton();
+                    });
                 });
             };
             var registerCancelButton = function() {
+                $("#" + config.cancelButton).click(function(e) {
+                    e.preventDefault();
+                    var rowId = $("#" + config.modelId).val();
+                    var selector = "#" + config.rowPrefix + rowId;
+                    if (rowId > 0) {
+                        $.get(config.getRow + rowId, {}, function(html) {
+                            $(selector).replaceWith(html);
+                        });
+                    } else {
+                        $(selector).remove();
+                    }
+                    showEditLinks();
+                });
             };
             var registerCreateButton = function() {
+                $("#" + config.createButton).click(function(e) {
+                    submitForm(this, e, config.postCreate, true);
+                });
             };
             var registerEditLinks = function() {
+                $("." + config.editLink).unbind("click");
                 $("." + config.editLink).click(function(e) {
                     e.preventDefault();
                     hideEditLinks();
@@ -56,8 +81,29 @@
                 });
             };
             var registerUpdateButton = function() {
+                $("#" + config.updateButton).click(function(e) {
+                    submitForm(this, e, config.putUpdate, false);
+                });
             };
-            var submitForm = function() {
+            var submitForm = function(element, event, url, isNew) {
+                event.preventDefault();
+                var formData = $("#" + config.formId).serialize();
+                var rowId = $("#" + config.modelId).val();
+                var selector = "#" + config.rowPrefix + rowId;
+                var requestType = (isNew || config.usePostUpdate) ? "post" : "put";
+                $.ajax({
+                    type: requestType,
+                    url: url,
+                    data: formData,
+                    dataType: "html",
+                    success: function(html) {
+                        $(selector).replaceWith(html);
+                        var flashRow = isNew ? "tbody tr:last" : selector;
+                        if (config.useHighlight) $(flashRow).effect("highlight", {}, 3000);
+                        showEditLinks();
+                        registerEditLinks();
+                    }
+                });
             };
             var showEditLinks = function() {
                 $("#" + config.addLink).parents("tr").show();
@@ -70,28 +116,7 @@
             prepConfig();
             ensureFormExists();
             registerEditLinks();
+            registerAddLink();
         });
     };
-
-    function registerEditLinks(config) {
-        var selector = "." + config.editLink;
-        $(selector).click(function(e) {
-            e.preventDefault();
-            hideEditLinks();
-            var row = $(this).parents("tr");
-            var rowId = row[0].id.match(/(\d+)/)[1];
-            $.get(config.getEdit + "/" + rowId, {}, function(html) {
-                row.replaceWith(html);
-                registerUpdateButton();
-                registerCancelButton();
-            });
-        });
-    };
-
-    function hideEditLinks() {
-    };
-
-    function showEditLinks() {
-    };
-
 })(jQuery);
